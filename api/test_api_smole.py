09e5433d556a0c5a85ae42d43fbe4961a48e882e
ADMehta <apeksha.d.mehta@gmail.com>
2025-08-06 19:58:34 -0700

    Delete api directory

--- api/test_api_smole.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def test_signup_api():
-    url = "https://my.functionhealth.com/signup?code=928AA4E1CD199B9D73A1A3B7DBC7F4F7"
-    response = requests.get(url)
-    assert response.status_code == 200
